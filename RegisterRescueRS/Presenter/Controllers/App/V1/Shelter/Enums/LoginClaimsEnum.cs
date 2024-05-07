3600fc6053d9450705a23c0ec0945b622317f363
Caio Miguel Fernandes <caiombuenof@gmail.com>
2024-05-06 22:39:04 -0300

    Partial commit dnv

--- RegisterRescueRS/Presenter/Controllers/App/V1/Shelter/Enums/LoginClaimsEnum.cs
+++
@@ -1,6 +0,0 @@
-namespace RescueRS.Presenter.Controllers.App.V1.Enums;
-
-public static class LoginClaimsEnum
-{
-    public static string ShelterId = "ShelterId";
-}
--- RegisterRescueRS/Presenter/Controllers/App/V1/Shelter/ShelterController.cs
+++ RegisterRescueRS/Presenter/Controllers/App/V1/Shelter/ShelterController.cs
@@ -6,11 +6,11 @@ using RegisterRescueRS.Presenter.Controllers.App.V1.DTOs;
 namespace RegisterRescueRS.Presenter.Controllers.App.V1;
 
 [ApiController]
-[Route("api/v{version:apiVersion}/Login")]
+[Route("api/v{version:apiVersion}/Shelter")]
 [ApiVersion("1.0")]
 public class ShelterController(IServiceProvider serviceProvider) : BaseController(serviceProvider)
 {
-    [HttpPost("CreateShelter")]
+    [HttpPost]
     [MapToApiVersion("1.0")]
     public async Task<IResponse<ResponseDTO>> CreateShelter(CreateShelterDTO dto) =>
         await this.serviceProvider.GetRequiredService<ShelterService>()
