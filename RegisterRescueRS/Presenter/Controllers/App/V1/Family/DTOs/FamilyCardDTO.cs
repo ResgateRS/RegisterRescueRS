10d19f47c64724951bdbebc218ef6e6d8d21233d
Caio Miguel Fernandes <caiombuenof@gmail.com>
2024-05-15 15:09:01 -0300

    Shelter into FamilyCardDTO

--- RegisterRescueRS/Presenter/Controllers/App/V1/Family/DTOs/FamilyCardDTO.cs
+++ RegisterRescueRS/Presenter/Controllers/App/V1/Family/DTOs/FamilyCardDTO.cs
@@ -6,18 +6,19 @@ public class FamilyCardDTO
     public Guid FamilyId { get; set; }
     public int TotalPeopleNumber { get; set; }
     public string Responsable { get; set; } = null!;
+    public string? Shelter { get; set; }
     public string? Cellphone { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
     public DateTimeOffset UpdatedAt { get; set; }
 
-    // TODO: Update DTO attributes to expose shelter and shelter address properly
     internal static FamilyCardDTO FromEntity(FamilyEntity entity) =>
         new()
         {
             FamilyId = entity.FamilyId,
-            Responsable = $"{entity.Shelter.ShelterName} - {entity.Shelter.Address}",
-            Cellphone = $"{entity.Houseds.First(x => x.IsFamilyResponsable).Name} - {entity.Houseds.First(x => x.IsFamilyResponsable).Cellphone}",
+            Responsable = entity.Houseds.First(x => x.IsFamilyResponsable).Name,
+            Cellphone = entity.Houseds.First(x => x.IsFamilyResponsable).Cellphone,
+            Shelter = $"{entity.Shelter.ShelterName} - {entity.Shelter.Address}",
             TotalPeopleNumber = entity.Houseds.Count(x => x.Active),
             Latitude = entity.Shelter.Latitude,
             Longitude = entity.Shelter.Longitude,
